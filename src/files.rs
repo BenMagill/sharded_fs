ea4847c6558047cdf168f205257fb45a4acc97a4
benmagill <benmagill@protonmail.com>
2023-04-10 12:33:37 +0100

    clean up

--- src/files.rs
+++ src/files.rs
@@ -1,17 +1,25 @@
 use std::{fs::{self, rename, copy}, path::{Path, PathBuf}, io::Error};
 
 use actix_multipart::{form::{MultipartForm, text::Text, tempfile::{TempFile, TempFileConfig}}};
-use actix_web::{web, Responder};
+use actix_web::{web::{self, Data}, Responder, HttpRequest};
+
+struct FilesConfig {
+    storage_folder: PathBuf,
+    temp_folder: PathBuf,
+}
 
 async fn index() -> impl Responder {
     "Hello!"
 }
 
 // TODO: stream result for large files? 
-async fn read(path: web::Path<String>) -> impl Responder {
+async fn read(req: HttpRequest, path: web::Path<String>) -> impl Responder {
+    let config = req.app_data::<Data<FilesConfig>>()
+        .unwrap();
     let file_name = path.into_inner();
-
-    return match read_from_file(file_name.as_str()) {
+    
+    let result = read_from_file(get_path(config, file_name.as_str()));
+    return match result {
         Ok(contents) => { contents },
         Err(_) => { "Could not find file".as_bytes().to_vec() },
     };
@@ -23,16 +31,18 @@ struct FileUpload {
     file: TempFile,
 }
 
-async fn write(form: MultipartForm<FileUpload>) -> impl Responder {
+async fn write(req: HttpRequest, form: MultipartForm<FileUpload>) -> impl Responder {
+    let config = req.app_data::<Data<FilesConfig>>()
+        .unwrap();
     
     println!("{}", form.file_name.0);
     println!("{}", form.file.file.path().to_str().unwrap());
     let temp_name = form.file.file.path();
-    let path = get_path(form.file_name.0.as_str());
+    let path = get_path(config, form.file_name.0.as_str());
 
     return match move_file(temp_name, path) {
         Ok(no_copy) => {
-            if (no_copy) { println!("Could not move file, had to copy") };
+            if no_copy { println!("Could not move file, had to copy") };
             "Ok"
         },
         Err((e1, e2)) => {
@@ -43,20 +53,28 @@ async fn write(form: MultipartForm<FileUpload>) -> impl Responder {
     };
 }
 
-fn get_path(filename: &str) -> PathBuf {
-    return Path::new("files").join(filename);
+// fn get_temp_folder() -> PathBuf {
+//     Path::new("temp").to_owned()
+// }
+
+// fn get_storage_folder() -> PathBuf {
+//     Path::new("files").to_owned()
+// }
+
+fn get_path(config: &Data<FilesConfig>, filename: &str) -> PathBuf {
+    return config.storage_folder.join(filename);
 }
 
+/**
+ * Ok(bool): true when used fs move, false when had to copy 
+ */
 fn move_file<T: AsRef<Path>, S: AsRef<Path>>(from: T, to: S) -> Result<bool, (Error, Error)> {
     return match rename(&from, &to) {
         Ok(_) => { Ok(true) },
         Err(e) => { 
-            // println!("{}", e.to_string());
-            // println!("Having to copy file instead of move");
             return match copy(&from, &to) {
                 Ok(_) => { Ok(false) },
                 Err(e2) => {
-                    // println!("{}", e.to_string());
                     Err((e, e2))
                 }
             };
@@ -64,16 +82,23 @@ fn move_file<T: AsRef<Path>, S: AsRef<Path>>(from: T, to: S) -> Result<bool, (Er
     };
 }
 
-fn read_from_file(filename: &str) -> std::io::Result<Vec<u8>> {
-    // TODO: safely read
-    let contents = fs::read(get_path(filename));
+fn read_from_file(path: PathBuf) -> std::io::Result<Vec<u8>> {
+    let contents = fs::read(path);
     return contents;
 }
 
 pub fn file_config(cfg: &mut web::ServiceConfig) {
+    let config = FilesConfig {
+        storage_folder: Path::new("files").to_owned(),
+        temp_folder: Path::new("temp").to_owned()
+    };
+    
     let multipart_data = TempFileConfig::default()
-        .directory(Path::new("temp").to_path_buf());
+        .directory(&config.temp_folder);
 
+    let a = Data::new(config);
+
+    cfg.app_data(Data::clone(&a));
     cfg.service(
         web::resource("/")
             .route(web::get().to(index))
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod files;
-use actix_web::{App, HttpServer, web};
+use std::{path::{PathBuf, Path}, sync::{Arc, Mutex}};
+
+use actix_web::{App, HttpServer, web::{self, Data}};
 use files::file_config;
 use futures;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let server = HttpServer::new(|| {
+    let server = HttpServer::new(move || {
         App::new()
             .service(web::scope("/files").configure(file_config))
     })
@@ -16,13 +18,13 @@ async fn main() -> std::io::Result<()> {
     // TODO: this would ideally be a lower level 
     let other_server = HttpServer::new(|| {
         App::new()
-            .service(web::scope("/files").configure(file_config))
+            // .service(web::scope("/files").configure(file_config))
     })
         .bind(("127.0.0.1", 8081))
         .unwrap()
         .run();
     
-    futures::try_join!(server, other_server);
+    futures::join!(server, other_server);
 
     return Ok(());
 }
